a419403341c08cbbeaad95bb1f6001d0b05f9a59
D-numa31415 <numa0402d@gmail.com>
2024-10-04 03:58:17 +0900

    Add files via upload

---
+++ G-image.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+import glob
+
+print("処理開始")
+in_img = glob.glob(r'.\in-image\*.ppm')
+for i in range(len(in_img)):
+    img = cv2.imread(in_img[i])
+    img[:,:,0] = 0
+    img[:,:,2] = 0
+    cv2.imwrite(f"./outG-image/{i:02}.ppm",img)
+
+print("処理終了")
